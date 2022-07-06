ea9ac190653a4a26e2706059718bdfee14b94519
Karabo Sambo <karabosambo@outlook.com>
2022-07-06 12:32:30 +0200

    added firebase hosting

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDLuYTyzcYfCwtOvVqMyTHBgy59vcgMmkI",
+  authDomain: "fem-iptracker.firebaseapp.com",
+  projectId: "fem-iptracker",
+  storageBucket: "fem-iptracker.appspot.com",
+  messagingSenderId: "325681045786",
+  appId: "1:325681045786:web:d6eb58c3596214061b8ca3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
